2050c85d5b471bee32ca8acaf1386f70c24c7862
VladRost <s28288@pjwstk.edu.pl>
2024-04-11 13:29:09 +0200

    Completed task

---
+++ APBD4/WebApplication1/AnimalsConf/AnimalsService.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace WebApplication1;
+
+public class AnimalsService:IAnimalService
+{
+    private static List<Animal> _animals = new List<Animal>() { new Animal(1,"adf",2.1,"test","czerwony"),new Animal(2,"adasdff",2.6,"test2","zolty")};
+    public IEnumerable<Animal> GetAnimals()
+    {
+        return _animals;
+    }
+
+    public Animal GetAnimalById(int id)
+    {
+        return _animals.FirstOrDefault(a => a.Id == id);
+    }
+    public Animal AddAnimal(Animal animal){
+        _animals.Add(animal);
+        return animal;
+    }
+    public Animal UpdateAnimal(int id,Animal updateAnimal){
+         var index = _animals.FindIndex(a => a.Id == id);
+        if (index != -1)
+        {
+            updateAnimal.Id = id;
+            _animals[index] = updateAnimal;
+            return updateAnimal;
+        }
+        else
+        {
+            return null;
+        }
+    }
+    public Animal DeleteAnimal(int id){
+        var animal = _animals.FirstOrDefault(a => a.Id == id);
+        _animals.Remove(animal);
+        return animal;
+    }
+}
---
+++ APBD4/WebApplication1/Configuration.cs
@@ -0,0 +1,48 @@
+﻿namespace WebApplication1;
+
+public static class Configuration
+{
+    public static void RegisterEndpointsForAnimals(this IEndpointRouteBuilder app)
+    {
+        app.MapGet("/api/v1/animals", (IAnimalService service) =>
+        {
+            
+            return TypedResults.Ok(service.GetAnimals());
+        });
+        app.MapGet("/api/v1/animals/{id}", (IAnimalService service) => { 
+            return TypedResults.Ok(service.GetAnimalById(1)); 
+        });
+        app.MapPost("/api/v1/animals",(IAnimalService service) =>
+        {
+            
+            return TypedResults.Ok(service.AddAnimal(new Animal(3,"ptak",3,"temp","niebeski")));
+        });
+        app.MapPut("/api/v1/animals/{id}",(IAnimalService service) =>
+        {
+            
+            return TypedResults.Ok(service.UpdateAnimal(1,new Animal(1,"KON",342.23,"animal","czarny")));
+
+        });
+        app.MapDelete("/api/v1/animals/{id}",(IAnimalService service) =>
+        {
+            
+            return TypedResults.Ok(service.DeleteAnimal(1));
+
+        });
+        app.MapGet("/api/v1/visits/{animalId}",(IVisitService service) =>
+        {
+            
+            return TypedResults.Ok(service.GetAnimalVisits(2));
+
+        });
+        app.MapPut("/api/v1/visits",(IVisitService service) =>
+        {
+            
+            return TypedResults.Ok(service.CreateVisit(new Visit(3,"1092",2,"test visit 221",453.25)));
+
+        });
+
+
+
+    }
+}
